393b82d96a2010387514e44f59def4ce238d5897
violachyu <pinckychyu@gmail.com>
2023-12-21 14:33:10 +0800

    Revert "Add unnecessary files"
    This reverts commit 68b53c1661111d2dc4bd4b6fa968ec591a7d4459.

--- app/routes.py
+++ app/routes.py
@@ -1,45 +1,49 @@
 # app/routes.py
 from flask import Flask, jsonify, request
 from app import app, db, redis_store
-from app.models import ShippingInfo, TrackingEvent, Recipient, Location
+from app.models import TrackingInfo, TrackingEvent
+from faker import Faker
+
+fake = Faker()
 
 # A. Query API
 @app.route('/query', methods=['GET'])
 def query_tracking():
     sno = request.args.get('sno')
-    tracking_info = ShippingInfo.query.filter_by(ShippingInfo_SNO=sno).first()
+
+    tracking_info = TrackingInfo.query.filter_by(sno=sno).first()
 
     if tracking_info:
-        details = TrackingEvent.query.filter_by(TrackingEvent_ShippingInfoSNO=sno).all()
-        recipient = Recipient.query.get(tracking_info.ShippingInfo_RecipientId)
-        current_location = Location.query.get(details[-1].TrackingEvent_LocationId) if details else None
+        details = TrackingEvent.query.filter_by(sno=sno).all()
+        recipient = tracking_info.recipient
+        current_location = tracking_info.current_location
 
         response_data = {
-            "sno": tracking_info.ShippingInfo_SNO,
-            "tracking_status": tracking_info.ShippingInfo_status,
-            "estimated_delivery": str(tracking_info.ShippingInfo_estimated_delivery),
+            "sno": tracking_info.sno,
+            "tracking_status": tracking_info.tracking_status,
+            "estimated_delivery": str(tracking_info.estimated_delivery),
             "details": [
                 {
-                    "id": event.TrackingEvent_Id,
-                    "date": str(event.TrackingEvent_Date),
-                    "time": str(event.TrackingEvent_Time),
-                    "status": event.TrackingEvent_status,
-                    "location_id": event.TrackingEvent_LocationId,
-                    "location_title": Location.query.get(event.TrackingEvent_LocationId).Location_Title
+                    "id": event.id,
+                    "date": str(event.date),
+                    "time": str(event.time),
+                    "status": event.status,
+                    "location_id": event.location_id,
+                    "location_title": event.location_title
                 }
                 for event in details
             ],
             "recipient": {
-                "id": recipient.Recipient_Id,
-                "name": recipient.Recipient_Name,
-                "address": recipient.Recipient_Address,
-                "phone": recipient.Recipient_Phone
+                "id": recipient.id,
+                "name": recipient.name,
+                "address": recipient.address,
+                "phone": recipient.phone
             },
             "current_location": {
-                "location_id": current_location.Location_Id if current_location else None,
-                "title": current_location.Location_Title if current_location else None,
-                "city": current_location.Location_City if current_location else None,
-                "address": current_location.Location_Address if current_location else None
+                "location_id": current_location.id,
+                "title": current_location.title,
+                "city": current_location.city,
+                "address": current_location.address
             }
         }
 
@@ -56,9 +60,9 @@ def generate_fake_data():
     fake_data = []
 
     for _ in range(num):
-        tracking_info = ShippingInfo(
-            # sno=fake.unique.random_number(digits=10),  # Generating a random 10-digit number as sno
-            # tracking_status=fake.random_element(elements=('In Transit', 'Out for Delivery', 'Delivered'))
+        tracking_info = TrackingInfo(
+            sno=fake.unique.random_number(digits=10),  # Generating a random 10-digit number as sno
+            tracking_status=fake.random_element(elements=('In Transit', 'Out for Delivery', 'Delivered'))
         )
         db.session.add(tracking_info)
         fake_data.append({"sno": tracking_info.sno, "tracking_status": tracking_info.tracking_status})
@@ -67,3 +71,7 @@ def generate_fake_data():
     # redis_store.delete('shipments_cache')  # Clear cache
 
     return jsonify(fake_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
